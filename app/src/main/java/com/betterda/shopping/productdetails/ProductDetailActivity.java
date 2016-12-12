d4ec6973d0cd360cb5d15252d85e8c9a778e346b
totcw <totcw@qq.com>
2016-12-12 17:33:13 +0800

    修改了商品详情的着色

--- app/src/main/java/com/betterda/shopping/productdetails/ProductDetailActivity.java
+++ app/src/main/java/com/betterda/shopping/productdetails/ProductDetailActivity.java
@@ -1,9 +1,11 @@
 package com.betterda.shopping.productdetails;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -11,6 +13,7 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.betterda.mylibrary.Utils.StatusBarCompat;
 import com.betterda.shopping.R;
 import com.betterda.shopping.base.BaseActivity;
 import com.betterda.shopping.productdetails.contract.ProductDetailContract;
@@ -42,6 +45,10 @@ public class ProductDetailActivity extends BaseActivity<ProductDetailContract.Pr
     FrameLayout mFrameProductdetailContent;
     @BindView(R.id.linear_productdetail_content)
     LinearLayout mLinearProductdetailContent;
+    @BindView(R.id.linear)
+    LinearLayout linear;
+    @BindView(R.id.view)
+    View viewL;
 
     private int height;
     private int height2;
@@ -57,14 +64,34 @@ public class ProductDetailActivity extends BaseActivity<ProductDetailContract.Pr
     @Override
     public void initView() {
         super.initView();
-
         setContentView(R.layout.activity_productdetail);
-
-        mScreenHeight = UtilMethod.getHeight(getmActivity())-UtilMethod.statusHeight(getmActivity());
-
 
     }
 
+    @Override
+    public void init() {
+        super.init();
+        //1. 首先将手机手机状态栏透明化：
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.height = UtilMethod.statusHeight(getmActivity());
+            viewL.setLayoutParams(layoutParams);
+
+
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
+
+        mScreenHeight = UtilMethod.getHeight(getmActivity());
+
+    }
 
     @Override
     public void initListener() {
@@ -78,11 +105,11 @@ public class ProductDetailActivity extends BaseActivity<ProductDetailContract.Pr
             }
         });
 
-        ViewTreeObserver vto2 = mRelativeProductdetailTitle.getViewTreeObserver();
+        ViewTreeObserver vto2 = linear.getViewTreeObserver();
         vto2.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
             @Override
             public void onGlobalLayout() {
-                height2 = mRelativeProductdetailTitle.getHeight();
+                height2 = linear.getHeight();
 
             }
         });
@@ -121,6 +148,8 @@ public class ProductDetailActivity extends BaseActivity<ProductDetailContract.Pr
 
         if (y <= 0) {   //设置标题的背景颜色
             mRelativeProductdetailTitle.setBackgroundColor(Color.argb( 0, 255, 255, 255));
+            viewL.setBackgroundColor(Color.argb( 0, 255, 255, 255));
+
             com.betterda.mylibrary.Utils.StatusBarCompat.setStatusBar5(getmActivity(),R.color.transparent);
         } else if (y > 0 && y <= height - height2) { //滑动距离小于banner图的高度时，设置背景和字体颜色颜色透明度渐变
 
@@ -128,15 +157,24 @@ public class ProductDetailActivity extends BaseActivity<ProductDetailContract.Pr
             float alpha = (255 * scale);
             mTvProductdetailTitle.setTextColor(Color.argb((int) alpha, 144, 151, 166));
             mRelativeProductdetailTitle.setBackgroundColor(Color.argb((int) alpha, 255, 255, 255));
+            viewL.setBackgroundColor(Color.argb((int) alpha, 255, 255, 255));
             if (y == height - height2) {
-                com.betterda.mylibrary.Utils.StatusBarCompat.setStatusBar5(getmActivity(),R.color.white);
+                viewL.setBackgroundColor(Color.argb(0, 255, 255, 255));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                    com.betterda.mylibrary.Utils.StatusBarCompat.setStatusBar5(getmActivity(), R.color.bg_color);
+                    // getWindow().setStatusBarColor(getResources().getColor(R.color.white));
+                }
+            } else {
+                com.betterda.mylibrary.Utils.StatusBarCompat.setStatusBar5(getmActivity(),R.color.transparent);
             }
 
 
         } else {    //滑动到banner下面设置普通颜色
-
+            com.betterda.mylibrary.Utils.StatusBarCompat.setStatusBar5(getmActivity(),R.color.transparent);
             // relatview.setBackgroundColor(Color.argb( 255, 144,151,166));
             mRelativeProductdetailTitle.setBackgroundColor(Color.argb(255, 255, 255, 255));
+            viewL.setBackgroundColor(Color.argb(255, 255, 255, 255));
         }
     }
 }
