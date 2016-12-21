7f76c3ec7056cd026ae46b49681679756614f50d
totcw <totcw@qq.com>
2016-12-21 15:12:33 +0800

    完成首页前面2个

--- app/src/main/java/com/betterda/shopping/shouye/ShouYeFragment.java
+++ app/src/main/java/com/betterda/shopping/shouye/ShouYeFragment.java
@@ -1,12 +1,27 @@
 package com.betterda.shopping.shouye;
 
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.betterda.mylibrary.LoadingPager;
+import com.betterda.mylibrary.Utils.StatusBarCompat;
 import com.betterda.shopping.R;
 import com.betterda.shopping.base.BaseFragment;
 import com.betterda.shopping.shouye.contract.ShouYeContract;
 import com.betterda.shopping.shouye.presenter.ShouYePresenterImpl;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Administrator on 2016/12/8.
@@ -15,13 +30,102 @@ import com.betterda.shopping.shouye.presenter.ShouYePresenterImpl;
 public class ShouYeFragment extends BaseFragment<ShouYeContract.Presenter> implements ShouYeContract.View {
 
 
+    @BindView(R.id.tv_shouye_city)
+    TextView mTvShouyeCity; //定位城市
+    @BindView(R.id.linear_location)
+    LinearLayout mLinearLocation;//定位
+    @BindView(R.id.frame_shouye_lunbotu)
+    FrameLayout mFrameShouyeLunbotu; //轮播图
+    @BindView(R.id.frame_shouye_second)
+    FrameLayout mFrameShouyeSecond;//第二区域
+    @BindView(R.id.frame_shouye_three)
+    FrameLayout mFrameShouyeThree;//第三区域
+    @BindView(R.id.loadpager_shouye)
+    LoadingPager mLoadpagerShouye;
+    private View mViewLunbotu; //轮播图
+    private View mViewSecond;//第二区域的view
+    private ViewPager mVpLunbotu;
+    private LinearLayout mLinearFirst;
+
     @Override
     public View initView(LayoutInflater inflater) {
-        return inflater.inflate(R.layout.fragment_shouye,null);
+        return inflater.inflate(R.layout.fragment_shouye, null);
     }
 
     @Override
     protected ShouYeContract.Presenter onLoadPresenter() {
         return new ShouYePresenterImpl();
     }
+
+    @Override
+    public void initData() {
+        super.initData();
+        //加载轮播图
+        initLunbotu();
+        //加载第二区域
+        initSecond();
+    }
+
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+        if (hidden) {//隐藏
+        } else {
+            StatusBarCompat.setStatusBar5(getmActivity(),R.color.backgroudyellow);
+
+        }
+    }
+
+
+    @OnClick({R.id.linear_location, R.id.relative_shouye_message, R.id.iv_shouye_search})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.linear_location:
+                break;
+            case R.id.relative_shouye_message:
+                break;
+            case R.id.iv_shouye_search:
+                break;
+        }
+    }
+
+    /**
+     * 初始化轮播图
+     */
+    private void initLunbotu() {
+        mViewLunbotu = View.inflate(getmActivity(), R.layout.view_lunbotu, null);
+        ImageView mIvFirst = (ImageView) mViewLunbotu.findViewById(R.id.iv_main_first);
+        mVpLunbotu = (ViewPager) mViewLunbotu.findViewById(R.id.vpager_lunbotu);
+        mLinearFirst = (LinearLayout) mViewLunbotu.findViewById(R.id.ll_points);
+        mFrameShouyeLunbotu.addView(mViewLunbotu);
+
+    }
+
+
+    private void initSecond() {
+        mViewSecond = View.inflate(getmActivity(), R.layout.view_second, null);
+        mFrameShouyeSecond.addView(mViewSecond);
+    }
+
+
+
+    /**
+     * 初始化轮播图的viewpager
+     * @param adapter
+     */
+    public void initVpLunbotu(PagerAdapter adapter) {
+        mVpLunbotu.setAdapter(adapter);
+    }
+
+    @Override
+    public ViewGroup getLpoint() {
+        return mLinearFirst;
+    }
+
+    @Override
+    public ViewPager getViewPager() {
+        return mVpLunbotu;
+    }
+
 }
--- app/src/main/java/com/betterda/shopping/shouye/contract/ShouYeContract.java
+++ app/src/main/java/com/betterda/shopping/shouye/contract/ShouYeContract.java
@@ -1,8 +1,16 @@
 package com.betterda.shopping.shouye.contract;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.betterda.shopping.base.IModel;
 import com.betterda.shopping.base.IPresenter;
 import com.betterda.shopping.base.IView;
+import com.betterda.shopping.shouye.model.LunBoTu;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/12/8.
@@ -11,12 +19,28 @@ import com.betterda.shopping.base.IView;
 public class ShouYeContract {
     
 public interface View extends IView{
+    //初始化轮播图的viewpage
+    void initVpLunbotu(PagerAdapter adapter);
+
+    ViewGroup getLpoint();
+
+    ViewPager getViewPager();
 }
 
 public interface Presenter extends IPresenter<View>{
+
+    //获取轮播图的数据
+    List<LunBoTu> getLunBoTuList();
+    //创建轮播图的点
+    void cratePoint();
+
+    void createHandler();
+    //创建轮播图的view
+    android.view.View ctreaImageView(int position);
 }
 
 public interface Model extends IModel{
+
 }
 
 
