9ec538421939fb07336e6d6909dbc5b1b3ced4f6
totcw <totcw@qq.com>
2017-02-24 12:45:03 +0800

    test6

--- app/src/main/java/com/betterda/shopping/my/MyFragment.java
+++ app/src/main/java/com/betterda/shopping/my/MyFragment.java
@@ -137,7 +137,7 @@ public class MyFragment extends BaseFragment<MyContract.Presenter> implements My
     public void onHiddenChanged(boolean hidden) {
         super.onHiddenChanged(hidden);
         if (hidden) {//隐藏
-
+            System.out.println("yinc");
         } else {
             StatusBarCompat.setStatusBar5(getmActivity(), R.color.backgroudyellow);
             ((MainActivity)getmActivity()).getmBvMain().setVisibility(View.GONE);
